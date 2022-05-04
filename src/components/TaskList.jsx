d1d0b7df9f6b818a6ccbc56c51f8641585e0124f
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-04 15:02:36 -0500

    Delete function working with DB. Project finished

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -44,31 +44,41 @@ const TaskList = () => {
             });  
     }
 
-    const onDelete = (note) => {
-        dispatch({
-            type: 'remove-note',
-            payload: note
-        })
+    const onDelete = async (note) => {
+        let response = await fetch(`http://localhost:8081/api/v1/notes/${note.id}`,
+        {
+            method: 'DELETE'
+        });
+
+        if (response.status === 200) {
+            dispatch({
+                type: 'remove-note',
+                payload: note
+            });
+        }; 
     }
 
-  return (
-    <div>
-        <h1>Your tasks</h1>
-        <ul>
-            {state.lstNotes.map(note => {
-                return (
-                <li key={note.id} className='task' style={note.done ? {textDecoration: 'line-through'} : {}}>
-                    <h3>{note.title} <BiTrashAlt onClick={() => onDelete(note)} /></h3>
-                    <h5>
-                        {note.message}
-                        <input className='form-control-check' onChange={(ev) => onCheckbox(ev, note)} type="checkbox" checked={note.done} />
-                    </h5>                    
-                </li>
-                )
-            })}
-        </ul>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Your tasks</h1>
+            <ul>
+                {state.lstNotes.map(note => {
+                    return (
+                    <li key={note.id} className='task' style={note.done ? {textDecoration: 'line-through'} : {}}>
+                        <h3>
+                            {note.title}
+                            <BiTrashAlt onClick={() => onDelete(note)} />
+                        </h3>
+                        <h5>
+                            {note.message}
+                            <input className='form-control-check' onChange={(ev) => onCheckbox(ev, note)} type="checkbox" checked={note.done} />
+                        </h5>                    
+                    </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default TaskList
